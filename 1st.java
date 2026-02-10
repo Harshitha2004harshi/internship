bddfa41b6e029fbf04dbfc613e60bae92ce04e0d
Harshitha H.M <hmharshitha909@gmail.com>
2026-02-10 10:34:44 +0530

    Create 1st.java

---
+++ 1st.java
@@ -0,0 +1,18 @@
+//Write a program to print the numbers from 10 to 50 using for loop/ while loop.//
+//Output = Negative//
+
+
+//FOR LOOP//
+
+public class negativenumbers {
+
+	public static void main(String[] args) {
+		for(int i=10;i<=50;i++) {
+			System.out.println(-i);
+		}
+
+	}
+
+}
+
+
