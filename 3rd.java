e077dfd51ca1c47719361b79a472384f255c8a1e
Harshitha H.M <hmharshitha909@gmail.com>
2026-02-10 10:36:57 +0530

    Create 3rd.java

---
+++ 3rd.java
@@ -0,0 +1,19 @@
+
+//Write a program to find the given number is negative or positive. Input = 123//
+
+
+public class positivenegative {
+
+	public static void main(String[] args) {
+		int num=123;
+		if(num>0) {
+			System.out.println("positive");
+		} else if(num<0) {
+			System.out.println("negative");
+		}else {
+			System.out.println("zero");
+		}
+
+	}
+
+}
